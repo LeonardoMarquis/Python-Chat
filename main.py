c504733fe8172df26d7cd99336860087e6318953
Leonardo <marquisleonardo12@gmail.com>
2025-02-28 15:09:57 -0300

    versao 2 - ler registros, escrever no txt etc

--- main.py
+++ main.py
@@ -3,25 +3,58 @@ import os;
 
 user = {
     "nome": input("Nome: "),
-    "idade": input("Idade: ")
+    "data": input("Data ex: 10/09/1932: ")
 }
 
 bd = open("bd-chat.txt", "a", encoding="utf-8")
-bd.write(user["nome"]);
-bd.write(user["idade"] + "\n");
+bd.write("\n" + user["nome"]);
+bd.write(user["data"] + "\n");
 
 mensagens = [];
 
 
-while True:
+#mostrar as mensagens já registradas
+def pergunta_registros():
+    print("-----------------------------------------------------------------------------------")
+    ver_registros = str(input("Ver registros anteriores?[yes][no] "));
+    if ver_registros == "yes":
+        os.system("cls");
+        with open("bd-chat.txt", "r", encoding="utf-8") as bd:
+            for line in bd:
+                print(line.strip())
+
+    while str.lower(ver_registros) != "yes" or "no":
+        print("-----------------------------------------------------------------------------------")
+        ver_registros = str.lower(str(input("Ver registros anteriores?[yes][no] ")));
+        if ver_registros == "yes":
+            os.system("cls");
+            with open("bd-chat.txt", "r", encoding="utf-8") as bd:
+                for line in bd:
+                    print(line.strip())
+
+        if ver_registros =="no":
+            break
+
+
+
+
+pergunta_registros();
+
+
+
+
+
+while True: 
     os.system("cls");
 
+    #mostrar a mensagem enviada antes de mandar outra
     if len(mensagens) > 0:
         for m in mensagens:
-            print(m['nome'] + "-" + m['texto'])
+            print(m['nome'] + ": " + m['texto'])
     print("-----------------------------------------------------------------------------------")
 
-    print("'fim' encerra a sessão")
+    print("Para encerrar a sessão digite 'fim' ")
+    print("Sua mensagem deverá ficar registrada, que seja uma boa mensagem.")
     texto = str(input("Digite sua mensagem: "))
 
     texto = str.lower(texto);
@@ -34,5 +67,5 @@ while True:
         "texto": texto
     }
     )
-    bd.write(user["nome"] + ":" + texto + "\n");
+    bd.write(user["nome"] + ": " + texto + "\n");
 
